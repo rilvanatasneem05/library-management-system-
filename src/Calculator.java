80ceb07166869f592c48398e773e48879e22b059
rilvana <rilvana005@gmail.com>
2026-02-13 18:06:50 +0530

    student management system

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 public class Calculator {
-    public static double add(double a, double b) {
+    public static double add(double a, double b)
+    {
         return a + b;
     }
-    public static double subtract(double a, double b) {
+    public static double subtract(double a, double b)
+
+    {
         return a - b;
     }
     public static double multiply(double a, double b) {
